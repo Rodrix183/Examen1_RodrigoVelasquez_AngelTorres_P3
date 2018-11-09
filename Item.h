9574e9d1552490e0c1d3fc78dd8e29d5f6345d1e
Angel Torres <ator89@outlook.com>
2018-11-09 13:59:15 -0600

    Prototipos

--- Item.h
+++ Item.h
@@ -1,15 +1,27 @@
 #ifndef ITEM_H
 #define ITEM_H
 
+#define SIZE 10
+
 class Item(){
     
     private: 
-        
+        int plataforma;
+        int vidas;
+        int golpes; 
+        Item*** tablero;
+ 
     public:
         Item();
         ~Item();
         
+        int*** crearTablero();
+        void llenarTablero();
+        void imprimirTablero();
+        void liberar();
 
+        void mover();
+        void iniciar();
 };
 
 #endif
