3b6a9c93cbaf52c3cae6601657050fa836e8430c
Angel Torres <ator89@outlook.com>
2018-11-09 20:40:21 -0600

    Objetos Item

--- Item.cpp
+++ Item.cpp
@@ -1,6 +1,8 @@
 #include "Item.h"
 //#include "ncurses.h"
 
+#include <iostream>
+
 Item::Item(){
     
     this->vidas = 3;
@@ -10,6 +12,7 @@ Item::Item(){
     this->nivel = 0;
     this->tipo = ' ';    
     this->tablero = nullptr;
+    this->matrix = nullptr;
 }
 
 Item::~Item(){
@@ -17,7 +20,7 @@ Item::~Item(){
 }
 
 
-Item::Item(int vidas , int golpes, int px, int py, int nivel, char tipo){
+Item::Item(int vidas, int golpes, int px, int py, int nivel, char tipo){
     this->vidas = vidas;
     this->golpes = golpes;
     this->px = px;
@@ -42,8 +45,64 @@ Item*** Item::crearTablero(){
     return tablero;
 }
 
+Item*** Item::createMatrix(){
+    matrix = new Item**[SIZE];
+    for( int i = 0; i < SIZE; i++){
+        matrix[i] = new Item*[SIZE];
+    }
+    for(int i = 0; i < SIZE; i++){
+        for( int j = 0; j < SIZE; j++){
+            matrix[i][j] = new Item[10];
+        }
+    }
+    return matrix;
+}
+
+Item*** Item::llenarMatrix(int nivel){
+    //this->nivel = nivel;
+    for(int i = 0; i < SIZE; i++){
+        for( int j = 0; j < SIZE; j++){
+            if(i <= 3){//lenar bloques
+                matrix[i][j] = new Item(3,0,i,j,nivel,'B');
+            }
+            if(i>3){//espacio vacio
+                matrix[i][j] = new Item(3,0,i,j,nivel, ' ' );
+            }
+            if( i ==9 && j>=3 && j<=7 ){
+                matrix[i][j] = new Item(3,0,i,j,nivel,'b');
+            }
+            
+        }
+    }
+    return matrix;
+}
+
+void Item::imprimirMatrix(){
+   for( int i= 0; i < SIZE; i++){
+        for(int j = 0; j < SIZE; j++){
+            if(matrix[i][j]->getTipo() == 'B'){
+                std::cout << getTipo() << ' ';
+                std::cout << "B";
+            }else{
+                //std::cout << std::endl;
+            }
+        }
+        //std::cout << std::endl;
+    } 
+}
+
+void Item::freeup(){
+    
+}
+
+
 void Item::llenar(){
-    /*for(int i =0; i < 10;i++){
+    /*for( int i =0; i < SIZE; i++){
+        for (int j = 0; j < SIZE; j++){
+            tablero[i][j] = new Item(0,1,i,j,1,'B');
+        } 
+    }
+    for(int i =0; i < 10;i++){
         for(int j = 0;j < 10;j++){
             if(i == 0){//blanco
                 tablero[i][j] = 'B';
@@ -110,6 +169,10 @@ void Item::mover(int c){
 */
 }
 
+
+
+
+
 void Item::liberar(){
     /*
     for(int i = 0; i < 10; i++){
@@ -135,6 +198,16 @@ void Item::liberar(){
         
     }while(vive);
 }*/
+
+
+
+
+
+
+
+
+
+
 
 //Mutadores y accesores
 void Item::setGolpe(int golpes){
--- temp.cpp
+++ temp.cpp
@@ -5,7 +5,10 @@
 int main(){
     
     Item* temp = new Item();
-
+    
+    temp->createMatrix();
+    temp->llenarMatrix(1);
+    temp->imprimirMatrix();
     temp->setX(3);
     std::cout << temp->getX() << std::endl;
     
